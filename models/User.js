a2f8bd8047a43610c6fcdf18e38e8546abdfcd2d
William Berry <76190311+wberry86@users.noreply.github.com>
2021-05-08 15:29:28 -0500

    fixed database connection issue

--- models/User.js
+++ models/User.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcrypt');
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
@@ -31,23 +32,30 @@ User.init(
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                lan: [4]
+                len: [4]
             }
         }
     },
     {
-        // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-
-        // pass in our imported sequelize connection (the direct connection to our database)
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+              updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+              return updatedUserData;
+            }
+          
+        
+        },
         sequelize,
-        // dont automatically create createdAt/updatedAt timestamp fields
         timestamps: false,
-        // don't pluralize name of database table 
         freezeTableName: true,
-        // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
         underscored: true,
-        // make it so our model name stays lowercase in the database
-        modelName: User
+        modelName: 'user'
     }
 );
 
